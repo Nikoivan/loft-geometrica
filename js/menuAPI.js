8ee3d9b8ba08386917ad8373be61b7319da21e9c
Niko Ivan <nixonivan@rambler.ru>
2023-08-09 09:12:31 +0300

    menu done

--- js/menuAPI.js
+++ js/menuAPI.js
@@ -1,41 +1,50 @@
 class MenuAPI {
-  constructor(btnName, menuName) {
+  constructor(btnName) {
     this.btn = document.querySelector(`.${btnName}`);
-    this.menu = document.querySelector(`.${menuName}`);
+    this.hamb = document.querySelector(".hamb__field");
     this.popup = document.querySelector("#popup");
 
     this.onBtnClick = this.onBtnClick.bind(this);
+    this.closeOnClick = this.closeOnClick.bind(this);
 
     this.btn.addEventListener("click", this.onBtnClick);
   }
 
   onBtnClick(e) {
     e.preventDefault();
-    const menu = document.querySelector("#menu").cloneNode(1);
-    this.activeMenu = menu;
+    const menu = document.createElement("ul");
+    menu.classList.add("menu");
+    menu.innerHTML = `<li><a href="index.html">Главная</a></li>                        
+    <li><a href="#">Услуги</a></li>            
+    <li><a href="#">Наши работы</a></li>
+    <li><a href="#">Контакты</a></li>`;
 
-    this.popup.classList.toggle("open");
-    this.btn.classList.toggle("active");
-    document.body.classList.toggle("noscroll");
-    this.renderPopup(menu);
+    document.body.classList.add("noscroll");
+    this.popup.classList.add("open");
+    this.hamb.classList.add("active");
+    this.popup.append(menu);
 
     const links = Array.from(menu.children);
 
     links.forEach((link) => {
       link.addEventListener("click", () => this.closeOnClick.call(this));
     });
+
+    this.activeMenu = menu;
+    this.btn.removeEventListener("click", this.onBtnClick);
+    this.btn.addEventListener("click", this.closeOnClick);
   }
 
-  renderPopup(menu) {
-    this.popup.append(menu);
-  }
-
-  closeOnClick() {
-    this.popup.classList.remove("open");
-    this.btn.classList.remove("active");
+  closeOnClick(e) {
+    e.preventDefault();
     document.body.classList.remove("noscroll");
+    this.popup.classList.remove("open");
+    this.hamb.classList.remove("active");
     this.activeMenu.remove();
     this.activeMenu = null;
+
+    this.btn.addEventListener("click", this.onBtnClick);
+    this.btn.removeEventListener("click", this.closeOnClick);
   }
 }
 
